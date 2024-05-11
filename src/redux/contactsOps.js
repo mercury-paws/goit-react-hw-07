eddad512a4359e6f0e3ef0b95ed0f070ba988361
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-05-11 17:31:37 +0200

    amend-action-name

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -5,10 +5,10 @@ axios.defaults.baseURL = "https://663b7266fee6744a6ea1ac17.mockapi.io";
 
 //Базовий тип екшену це рядок "contacts/fetchAll"
 export const fetchContacts = createAsyncThunk(
-  "fetchAll",
+  "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
+      const response = await axios.get("contacts");
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -18,10 +18,10 @@ export const fetchContacts = createAsyncThunk(
 
 //Базовий тип екшену це рядок "contacts/addContact"
 export const addContact = createAsyncThunk(
-  "addContact",
+  "contacts/addContact",
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post("/contacts", newContact);
+      const response = await axios.post("contacts", newContact);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,10 +31,10 @@ export const addContact = createAsyncThunk(
 
 //Базовий тип екшену це рядок "contacts/deleteContact"
 export const deleteContact = createAsyncThunk(
-  "deleteContact",
+  "contacts/deleteContact",
   async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${contactId}`);
+      const response = await axios.delete(`contacts/${contactId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
